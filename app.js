71faf84fb66ce9a66bc299d3190f6c1569d65100
Marcelo Almeida <contato@marceloalmeida.dev>
2020-09-10 23:20:32 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        produtos: [],
+        produto: false
+    },
+    filters: {
+        numeroPreco (valor) {
+            return valor.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+            });
+        }
+    },
+    methods: {
+        fetchProdutos () {
+            fetch('./api/produtos.json')
+                .then(response => response.json())
+                .then(response => this.produtos = response)
+        },
+        fetchProduto (id) {
+            fetch(`./api/produtos/${id}/dados.json`)
+                .then(response => response.json())
+                .then(response => this.produto = response);
+        },
+        abrirModal(id){
+            this.fetchProduto(id);
+            window.scrollTo({
+                top:0,
+                behavior: 'smooth'
+            })
+        },
+        fecharModal ({ target, currentTarget }) {
+            if (target === currentTarget) {
+                this.produto = false;
+            }
+        }
+    },
+    created () {
+        this.fetchProdutos()
+    }
+})
